700676caedf4fdd47e29ccdf33b29d98f5b403eb
myri <myri@posteo.de>
2019-05-21 19:52:00 +0200

    create events table in the db

--- db/migrate/20190512192507_create_events.rb
+++ db/migrate/20190512192507_create_events.rb
@@ -1,12 +1,10 @@
 class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
-      t.string :title
-      t.text :text
-      #start_date?
-      #end_date?
-      #location?
-      #coding_group_id?
+      t.string :title, null: false
+      t.text :description
+      t.datetime :start_time, null: false
+      t.datetime :end_time, null: false
 
       t.timestamps
     end
