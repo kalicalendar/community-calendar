6c277d7bb903a281573eb64380e46d7f3b89fbb9
myri <myri@posteo.de>
2019-05-27 21:05:27 +0200

    add delete method to controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,34 @@
 class EventsController  < ApplicationController
 
+	def new
+		@event = Event.new
+	end
+
+
+
+	def create
+ 		@event = Event.new(event_params)
+ 
+  		@event.save
+  		redirect_to @event
+	end
+
+
+	def destroy
+		@event = Event.find(params[:id])
+		@event.destroy
+
+		redirect_to events_path
+	end
+
+
+	private
+
+	def event_params
+		params.require(:event).permit(:description)      #richtig?
+	end
+	
+
+
+
 end
