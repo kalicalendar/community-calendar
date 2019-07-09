d51e1497692c4254a46c30583d3f05daf310b535
myri <myri@posteo.de>
2019-07-09 20:59:38 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'homepage/index'
+
+  resources :events
+
+  root 'homepage#index'
 end
