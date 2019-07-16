627a422103812fe2adb1adb8133f0dc50007f804
Kolja Lucht <kolja.lucht@bluereport.net>
2019-07-16 20:46:06 +0200

    Remove validations for now

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,6 @@
 # No calendar without an event\
 class Event < ApplicationRecord
   validates :title, presence: true
-  validates :start_time, presence: true
-  validates :end_time, presence: true
+  # validates :start_time, presence: true
+  # validates :end_time, presence: true
 end
